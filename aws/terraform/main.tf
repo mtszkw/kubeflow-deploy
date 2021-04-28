07ee9b08f992c1bb7d2aaf8ad1696bd88cb10154
mtszkw <mateusz.kwasniak@pm.me>
2021-04-28 04:50:51 +0200

    Add TF for EKS & VPC

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
+  profile = "personal-admin"
   region = "eu-central-1"
 }
 
@@ -181,4 +181,78 @@ resource "aws_iam_group_policy_attachment" "attach_IamLimited_to_group" {
 resource "aws_iam_group_policy_attachment" "attach_IamFullKF_to_group" {
   group      = aws_iam_group.group_KubeflowDevelopers.name
   policy_arn = aws_iam_policy.policy_IamFullAccessKubeflow.arn
+}
+
+#############################################################################################
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  cluster_name = "my-cluster"
+}
+
+# VPC
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.0.0"
+
+  name                 = "k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "15.1.0"
+
+  cluster_name    = "${local.cluster_name}"
+  cluster_version = "1.18"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 2
+      min_capacity     = 1
+
+      instance_type = "t3.small"
+    }
+  }
+
+  write_kubeconfig   = true
+  config_output_path = "./"
 }
